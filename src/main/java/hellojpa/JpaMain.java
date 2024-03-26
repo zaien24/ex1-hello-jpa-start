f6fa41ad7c822bb6cb52189afbc685bc0fc8d8df
yuyuhi <zaien24@naver.com>
2024-03-26 12:23:34 +0900

    JPQL

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -1,6 +1,11 @@
 package hellojpa;
 
-import jakarta.persistence.*;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
 
 public class JpaMain {
 
@@ -14,8 +19,15 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Member findMember = em.find(Member.class, 1L);
-            findMember.setName("HelloJPA");
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .setFirstResult(5)
+                            .setMaxResults(8)
+                            .getResultList();
+
+            for (Member member : result) {
+                System.out.println("member.name = " + member.getName());
+
+            }
 
             tx.commit();
         } catch (Exception e) {
