9eedd2440037ca375d2762343faef021be1699b4
yuyuhi <zaien24@naver.com>
2024-03-26 12:30:11 +0900

    섹션3 - 영속성 컨텍스트1

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,16 +19,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                            .setFirstResult(5)
-                            .setMaxResults(8)
-                            .getResultList();
+            // 비영속
+            Member member = new Member();
+            member.setId(100L);
+            member.setName("HelloJPA");
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
-
-            }
-
+            // 영속
+            System.out.println("=== BEFORE ===");
+            em.persist(member);
+            System.out.println("=== AFTER ===");
+            
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
